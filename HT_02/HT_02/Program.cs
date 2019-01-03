1f762d02c091079a99898bed7ea1ab0942b764c8
arthique <arthique@MacBook-Pro-arthique.local>
2019-01-03 20:10:23 +0300

    Realized check input number some conditions

--- HT_02/HT_02/Program.cs
+++ HT_02/HT_02/Program.cs
@@ -1,4 +1,4 @@
-﻿#define T01
+﻿#define T02
 using System;
 
 namespace HT_02
@@ -18,6 +18,23 @@ namespace HT_02
             number = Convert.ToInt32(Console.ReadLine());
             isDividedByThree = number % 3 == 0;
             Console.WriteLine($"A number {number} is {(isDividedByThree ? "" : "not ")}divided by 3");
+
+#endif
+            #endregion
+
+            #region T02
+#if (T02)
+
+            const string numberText = "Enter number: ";
+            int number;
+            bool isDividedByFiveWithReminderTwo;
+            bool isDividedBySevenWithReminderOne;
+
+            Console.Write(numberText);
+            number = Convert.ToInt32(Console.ReadLine());
+            isDividedByFiveWithReminderTwo = number % 5 == 2;
+            isDividedBySevenWithReminderOne = number % 7 == 1;
+            Console.WriteLine($"A number {number} is {(isDividedByFiveWithReminderTwo && isDividedBySevenWithReminderOne ? "" : "not ")}meets the conditions");
 
 #endif
             #endregion
