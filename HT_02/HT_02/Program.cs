13eaa8f8c75618b0794c186be448b82ae60f3787
arthique <arthique@MacBook-Pro-arthique.local>
2019-01-03 19:41:52 +0300

    Realized input number divided by 3

---
+++ HT_02/HT_02/Program.cs
@@ -0,0 +1,26 @@
+﻿#define T01
+using System;
+
+namespace HT_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region T01
+#if (T01)
+
+            const string numberText = "Enter number: ";
+            int number;
+            bool isDividedByThree;
+
+            Console.Write(numberText);
+            number = Convert.ToInt32(Console.ReadLine());
+            isDividedByThree = number % 3 == 0;
+            Console.WriteLine($"A number {number} is {(isDividedByThree ? "" : "not ")}divided by 3");
+
+#endif
+            #endregion
+        }
+    }
+}
