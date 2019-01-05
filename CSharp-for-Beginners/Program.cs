2384763786ec642170cca0577ed0fe0e05a3720d
arthique <arthique@MacBook-Pro-arthique.local>
2019-01-05 16:09:40 +0300

    Refactoring

---
+++ CSharp-for-Beginners/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_for_Beginners
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
