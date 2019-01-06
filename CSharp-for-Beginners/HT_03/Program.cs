838702ff889c04e665ad8f81b42d15c5581f26aa
arthique <arthique@MacBook-Pro-arthique.local>
2019-01-06 17:02:24 +0300

    Realized check input number divided by 3 and 7 with handle exceptions

--- CSharp-for-Beginners/HT_03/Program.cs
+++ CSharp-for-Beginners/HT_03/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#define T01
+using System;
 
 namespace HT_03
 {
@@ -6,7 +7,33 @@ namespace HT_03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region T01
+#if (T01)
+
+            const string numberText = "Enter a number: ";
+            int number;
+
+            Console.Write(numberText);
+            try
+            {
+                number = Int32.Parse(Console.ReadLine());
+                if (number % 3 == 0 && number % 7 == 0)
+                {
+                    Console.WriteLine($"The number {number} is diveded by 3 and 7");
+                }
+                else
+                {
+                    Console.WriteLine($"The number {number} is not divided by 3 and 7");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Entered value is not a number");
+            }
+
+
+#endif
+            #endregion
         }
     }
 }
