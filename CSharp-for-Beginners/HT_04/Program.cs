8b3df4e1df310402cf71cb73db2b3a8fd5b878c2
arthique <arthique@MacBook-Pro-arthique.local>
2019-01-07 12:35:55 +0300

    Realized array of numbers meets some condition

---
+++ CSharp-for-Beginners/HT_04/Program.cs
@@ -0,0 +1,42 @@
+﻿#define T01
+using System;
+
+namespace HT_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region T01
+#if (T01)
+
+            const string sizeText = "Enter array size: ";
+            int arraySize;
+            int[] numberArray;
+
+            try
+            {
+                Console.Write(sizeText);
+                arraySize = Int32.Parse(Console.ReadLine());
+                numberArray = new int[arraySize];
+                for (int i = 0, count = 0; count < arraySize; i++)
+                {
+                    if (i / 5 > 0 && i % 5 == 2)
+                    {
+                        numberArray[count] = i;
+                        Console.Write($"{(count > 0 ? $"\t{numberArray[count]}" : $"{numberArray[count]}")}");
+                        count++;
+                    }
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Entered value is not a number!");
+            }
+
+#endif
+            #endregion
+        }
+    }
+}
